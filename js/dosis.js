d191921d4054d03267d35a04bc9f6f4b947ba35d
mancst-dev <manuelcastano21@gmail.com>
2026-02-05 21:56:25 -0500

    Update
    JS:
    Se añade función para calcular desde un mismo botón
    
    Se añade función para borrar los datos contenidos dentro de los inputs
    
    Se añaden Validaciones
    
    Se añade formula para calcular el número de dosis por horas especificas.
    
    OTROS:
    Correcciones de errores y optimización.

--- js/dosis.js
+++ js/dosis.js
@@ -2,6 +2,10 @@ const searchInput = document.getElementById('searchInput');
 const optionsList = document.getElementById('optionsList');
 const options = optionsList.getElementsByTagName('li');
 
+//variables publicas
+
+
+
 //filtro de opciones input busqueda medicamentos
 searchInput.addEventListener('input', () => {
     const filter = searchInput.value.toLowerCase();
@@ -21,46 +25,86 @@ optionsList.addEventListener('click', (e) => {
     }
 });
 
-//funcion para calcular dosis por peso del peciente
-function calcularDosisKg(){
+//funcion para limpiar
+function cl() {
+    document.getElementById('peso').value = '';
+    document.getElementById('recomendada').value = '';
+    document.getElementById('presentacion').value = 'Selecciona uno';
+    document.getElementById('mgUnidad').value = '';
+    document.getElementById('prescrita').value = '';
+    document.getElementById('prescrita').value = '';
+    document.getElementById('disponible').value = '';
+    document.getElementById('volumen').value = '';
+    document.getElementById('resultadoKg').innerHTML = 'Esperando datos...';
+    document.getElementById('resultadoKg').style.color = 'black';
+    document.getElementById('resultadoDosis').innerHTML = 'Esperando datos...';
+    document.getElementById('resultadoDosis').style.color = 'black';
+    document.getElementById('resultadoVol').innerHTML = 'Esperando datos...';
+    document.getElementById('resultadoVol').style.color = 'black';
+}
+
+
+//funcion para bton calcular todo
+function calcularTodo() {
+    
+    //inputs de la interfaz
     const nombre = document.getElementById('searchInput').value || "el medicamento";
     const peso = parseFloat(document.getElementById('peso').value);
     const recomendada = parseFloat(document.getElementById('recomendada').value);
-    const displayResultadoKg = document.getElementById('resultadoKg');
-
-    const inputPrescrita = document.getElementById('prescrita');
-
-    if (isNaN(peso) || isNaN(recomendada) || recomendada <= 0) {
-        displayResultadoKg.innerHTML = "Por favor, Ingresa valores válidos.";
-        displayResultadoKg.style.color = "red";
-        return;
-    }
-
-    const dosisKg = peso * recomendada;
-
-    inputPrescrita.value = dosisKg;
-
-    displayResultadoKg.style.color = '#2c3e50';
-    // displayResultadoKg.innerHTML = `Dosis Prescrita: <strong>${dosisKg} mg</strong>`
-    displayResultadoKg.innerHTML = `Dosis de ${nombre}: <strong>${dosisKg} mg</strong>`
-}
-//funcion para realizar la conversion a volumen en ml del medicamento
-function calcularDosis() {
-    
-    const nombre = document.getElementById('searchInput').value || "el medicamento";
-    const prescrita = parseFloat(document.getElementById('prescrita').value);
     const disponible = parseFloat(document.getElementById('disponible').value);
     const volumen = parseFloat(document.getElementById('volumen').value);
-    const displayResultado = document.getElementById('resultadoVol');
+    const selectTiempo = document.getElementById('tiempoDosis');
+    
+    //resultados 
+    const displayKg = document.getElementById('resultadoKg');
+    const displayXToma = document.getElementById('resultadoDosis');
+    const displayVolumen = document.getElementById('resultadoVol');
+    const inputPrescrita = document.getElementById('prescrita');
+    const prescrita = parseFloat(document.getElementById('prescrita').value);
 
-    if (isNaN(prescrita) || isNaN(disponible) || isNaN(volumen) || disponible <= 0) {
-        displayResultado.innerHTML = "Por favor, ingresa valores válidos.";
-        displayResultado.style.color = "red";
-        return;
+    //validaciones
+
+
+    if (isNaN(peso) || isNaN(recomendada) ||recomendada <= 0) {
+        displayKg.innerHTML = "Por favor, Ingresa valores válidos.";
+        displayKg.style.color = "red";
+        
     }
 
-    const dosisfinal = (prescrita * volumen) / disponible;
+    if (isNaN(selectTiempo)){
+        displayXToma.innerHTML = "Esperando el resultado de la cantidad de dosis.";
+        displayXToma.style.color = "red";
+        
+    }
 
-    displayResultado.style.color = "#2c3e50";
-    displayResultado.innerHTML = `Administrar: <strong>${dosisfinal} ml de ${nombre}</strong>`;
+    if (isNaN(prescrita) || isNaN(disponible) || isNaN(volumen) || disponible <= 0) {
+        displayVolumen.innerHTML = "Por favor, ingresa valores válidos.";
+        displayVolumen.style.color = "red";
+        // return;
+    }
+
+    //calculos
+    //dosis diaria
+    const dosisKg = peso * recomendada;
+    inputPrescrita.value = dosisKg;
+    
+    //dosis por toma
+    const numeroDosis = parseInt(selectTiempo.value);
+    const horas = 24 / parseInt(numeroDosis);
+    const dosisPorTomaMg = dosisKg / numeroDosis;
+
+    //volumen ´por cada toma
+    const volumenPorTomaMl = (prescrita * volumen) / disponible;
+
+
+    //resultados
+    displayKg.style.color = "#2c3e50";
+    displayKg.innerHTML = `Dosis Total Diaria: <strong>${dosisKg.toFixed(2)} mg</strong>`;
+
+    displayXToma.style.color = "#2c3e50";
+    displayXToma.innerHTML = `${numeroDosis} dosis cada ${horas} horas en <strong>${dosisPorTomaMg.toFixed(2)} mg</strong> de <strong>${nombre}</strong>`;
+
+    displayVolumen.style.color = "#2c3e50";
+    displayVolumen.innerHTML = `Administrar: <strong>${volumenPorTomaMl.toFixed(2)} ml</strong> de ${nombre} por cada toma.`;
+
 }
